66b98446633175a929d31e95986ac5c37755ebd6
anna41 <annasapuzhak94@gmail.com>
2023-01-12 11:32:27 +0000

    test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World auto deploy test 2 23!';
+    return 'Hello World auto deploy test!';
   }
 }
